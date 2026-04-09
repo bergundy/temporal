a7ca41314c9253a784f4b2ebaa9f80545971cfbe
Roey Berman <roey.berman@gmail.com>
2026-04-08 23:00:25 -0700

    Fix workflow handler test for UpdatableTimer parameter

--- service/frontend/workflow_handler_test.go
+++ service/frontend/workflow_handler_test.go
@@ -194,6 +194,7 @@ func (s *WorkflowHandlerSuite) getWorkflowHandler(config *Config) *WorkflowHandl
 		scheduler.NewSpecBuilder(),
 		true,
 		nil, // Not testing activity handler here
+		nil, // Not testing updatable timer handler here
 		nil,
 		quotas.NoopRequestRateLimiter,
 	)
